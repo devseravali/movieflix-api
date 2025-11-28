a05e3c2046f0987a1be50fff36d9277f361484c6
devseravali <devseravali@gmail.com>
2025-11-28 19:51:01 -0300

    Atualização do projeto

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import { PrismaClient } from './generated/prisma/client';
 
+const prisma = new PrismaClient({});
 const port = 3000; 
 const app = express();
 
 app.get('/movies', (req, res) => { 
     res.send('listagem de filmes');
 });
+
+app.get('/movies', async (req, res) => {
+    const movies = await prisma.movies.findMany({
+        orderBy: { title: 'asc' },
+        include: { genres: true, languages: true },
+    });
+    res.json(movies);
+});  
 
 app.listen(port, () => {
     console.log(`Servidor em execução ${port}`);
