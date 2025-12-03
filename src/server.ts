e463a7354a992d7cec920b9fc726ad26f4bf404f
devseravali <devseravali@gmail.com>
2025-12-03 19:24:56 -0300

    Adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -23,22 +23,33 @@ app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } =
         req.body;
 
-try {
-    await prisma.movie.create({
-        data: {
-            title,
-            genre_id,
-            language_id,
-            oscar_count,
-            release_date: new Date(release_date),
-        },
-    });
+    try {
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: {
+                title: { equals: title, mode: "insensitive" },
+            },
+        });
 
-    res.status(201).send();
-} catch (error) {
-    return res.status(500).send({ message: "Falha ao cadastrar um filme" });
+        if (movieWithSameTitle) {
+            return res
+                .status(409)
+                .send({ message: "Já existe um filme com esse título" });
+        }
+
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        });
+
+        res.status(201).send();
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
     }
-  res.status(201).send();
 });
 
 app.listen(port, () => {
